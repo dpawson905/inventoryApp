d020d909c4e53c6cd6c927f46489c6e54989ad08
Darrell Pawson <dpawson905@gmail.com>
2018-05-12 22:20:01 -0400

    cleanup

--- middleware/index.js
+++ middleware/index.js
@@ -12,21 +12,3 @@ middlewareObj.isLoggedIn = function(req, res, next) {
 };
 
 module.exports = middlewareObj;
-
-
-
-
-// .post(function(req, res) {
-//         var item = new Item();
-//         item.name = req.body.name;
-//         item.description = req.body.description;
-//         item.price = req.body.price;
-
-//         item.save(function(err) {
-//             if (err) {
-//                 res.send(err);
-//             }
-//             res.json({message: "Item was successfully saved"});
-//             console.log(item);
-//         })
-//     });

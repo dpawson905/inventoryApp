e7fc39fb7bc1aab05a34240f349f647255f385ad
Darrell Pawson <dpawson905@gmail.com>
2018-05-13 16:26:46 -0400

    fix user file

--- routes/user.js
+++ routes/user.js
@@ -3,69 +3,68 @@ var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 
-router
-  .route("/login")
-  .get(function(req, res) {
-    res.render("auth/login");
-  })
+router.get("/login", function(req, res) {
+  res.render("auth/login");
+});
 
-  .post(function(req, res, next) {
-    passport.authenticate("local", function(err, user) {
+router.post("/login", function(req, res, next) {
+  passport.authenticate("local", function(err, user) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      req.flash("error", "The username or password was incorrect");
+      return res.redirect("/");
+    }
+    req.logIn(user, function(err) {
       if (err) {
         return next(err);
       }
-      if (!user) {
-        req.flash("error", "The username or password was incorrect");
-        return res.redirect("/");
-      }
-      req.logIn(user, function(err) {
-        if (err) {
-          return next(err);
-        }
-        var redirectTo = req.session.redirectTo
-          ? req.session.redirectTo
-          : "/products/inventory/" + user._id;
-        delete req.session.redirectTo;
-        res.redirect(redirectTo);
-      });
-    })(req, res, next);
-  });
-
-router
-  .route("/register")
-  .get(function(req, res) {
-    res.render("auth/register");
-  })
-
-  .post(function(req, res) {
-    var newUser = new User({
-      username: req.body.username,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      loggedIn: false
+      var redirectTo = req.session.redirectTo
+        ? req.session.redirectTo
+        : "/products/inventory/" + user._id;
+      delete req.session.redirectTo;
+      res.redirect(redirectTo);
     });
+  })(req, res, next);
+});
 
-    if (req.body.adminCode === process.env.ADMIN_CODE) {
-      newUser.isAdmin = true;
+router.get("/register", function(req, res) {
+  res.render("auth/register");
+});
 
-      User.register(newUser, req.body.password, function(err, user) {
-        if (err) {
-          req.flash("error", err.message);
-          res.redirect("auth/register");
-        }
-        passport.authenticate("local")(req, res, function() {
-          req.flash("success", "Welcome to BCP " + user.username);
-          res.redirect("/products/inventory/" + user._id);
-        });
-      });
-    } else {
-      req.flash("error", "You must have the supplied admin code to create an account");
-      res.redirect("/");
-    }
+router.post("/register", function(req, res) {
+  var newUser = new User({
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    loggedIn: false
   });
 
-router.route("/logout").get(function(req, res) {
+  if (req.body.adminCode === process.env.ADMIN_CODE) {
+    newUser.isAdmin = true;
+
+    User.register(newUser, req.body.password, function(err, user) {
+      if (err) {
+        req.flash("error", err.message);
+        res.redirect("auth/register");
+      }
+      passport.authenticate("local")(req, res, function() {
+        req.flash("success", "Welcome to BCP " + user.username);
+        res.redirect("/products/inventory/" + user._id);
+      });
+    });
+  } else {
+    req.flash(
+      "error",
+      "You must have the supplied admin code to create an account"
+    );
+    res.redirect("/");
+  }
+});
+
+router.get("/logout",function(req, res) {
   req.logout();
   req.flash("success", "Logged You Out");
   res.redirect("/");

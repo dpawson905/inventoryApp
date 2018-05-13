e71328657f3382955b77de08b0cac9cb22a8745f
Darrell Pawson <dpawson905@gmail.com>
2018-05-13 17:41:42 -0400

    update delete route

--- routes/items.js
+++ routes/items.js
@@ -202,17 +202,8 @@ router.post("/item/:id", middleware.isLoggedIn, async (req, res) => {
 
 router.delete("/item/:id", middleware.isLoggedIn, async(req, res) => {
   try {
-    let item = await Item.findById(req.params.id);
     User.update({ _id: req.user._id }, { $pull: { items: req.params.id } });
-    fs.unlinkSync(__dirname + "../../public" + item.image);
-    await Item.findByIdAndRemove(req.params.id);
-    fs
-      .createReadStream(__dirname + "../../public/uploads/backup/no-image.png")
-      .pipe(
-        fs.createWriteStream(
-          __dirname + "../../public/uploads/no-img/no-image.png"
-        )
-      );
+    Item.findByIdAndRemove(req.params.id);
     req.flash("Success", "Item Deleted");
     res.redirect("back");
   } catch (e) {
